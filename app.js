9cc37ed3b04e98f43525562da6828c7888c4c845
Ben Jaffe <jaffe.ben@gmail.com>
2014-07-10 08:02:38 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 var express = require('express');
+var http = require('http');
+var app = express();
+
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// http routes
+// var routes = require('./routes/index');
 
-var app = express();
+var songs = require('./songs.json');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(express.Router());
+
+app.get('/',function(req,res) {
+    res.render('index', {
+        title: 'Songs'
+    });
+});
+
+app.get('/songs/:songid?', function(req, res) {
+    var song = songs[req.params.songid];
+    res.render('index', {
+        title: 'Songs - ' + song.title,
+        currentSong: song
+    });
+});
 
 
 app.locals.songs = require('./songs.json');
 
-app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
