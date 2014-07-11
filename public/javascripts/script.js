7f3e97bdb70f191000f39d240727e967ea68c8c4
Ben Jaffe <jaffe.ben@gmail.com>
2014-07-11 11:21:07 -0700

    scroll logic improvements, scroll animation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,7 @@ var socket = io.connect();
 var songIndex = location.pathname.split('songs/')[1];
 var scrollPos = [0,0];
 var state = {};
-
+var scrollDisabled = false;
 
 
 socket.on('connect', function(client){
@@ -13,7 +13,6 @@ socket.on('connect', function(client){
 	state.timestamp = Date.now();
 	state.songIndex = songIndex;
 	state.scrollPos = scrollPos;
-	state.justAutoScrolled = false;
 
 	socket.on('id',function(id) {
 		state.clientid = id;
@@ -22,18 +21,16 @@ socket.on('connect', function(client){
 	socket.emit('ready', state);
 
 	document.addEventListener('scroll', function(e) {
-		// console.log(state.justAutoScrolled);
-		if (!state.justAutoScrolled) {
+		if (scrollDisabled) return false;
 
-			var scrollPos = {x: window.scrollX, y: window.scrollY};
-			var data = {
-				timestamp: Date.now(),
-				scrollPos: scrollPos,
-				clientid: socket.id
-			};
-			socket.emit('scroll', data);
-			// console.log('Sending scroll position');
-		}
+		var scrollPos = {x: window.scrollX, y: window.scrollY};
+		var data = {
+			timestamp: Date.now(),
+			scrollPos: scrollPos,
+			clientid: socket.id
+		};
+		socket.emit('scroll', data);
+
 	});
 
 
@@ -47,30 +44,43 @@ socket.on('connect', function(client){
 	socket.on('scroll', function(data){
 		// console.log('SOMEONE ELSE IS SCROLLING!   ' + data.clientid + '   ' + state.clientid);
 		// console.log(Date.now() - data.timestamp);
-		state.justAutoScrolled = true;
+		scrollDisabled = true;
 		setTimeout(function(){
-			state.justAutoScrolled = false;
+			scrollDisabled = false;
 		},0);
 
 		var scrollPos = data.scrollPos;
-		window.scrollTo(scrollPos.x, scrollPos.y);
-		// animateScrollTo(scrollPos.x, scrollPos.y);
+		// window.scrollTo(scrollPos.x, scrollPos.y);
+		animateScrollTo(scrollPos.x, scrollPos.y);
 		// console.log('Receiving scroll position ' + scrollPos.y);
 	});
 
 });
+var interval;
+var yTarget;
+var maxScrollSpeed = 60;
+var animateScrollTo = function(x,y) {
+	yTarget = y;
+	scrollDisabled = true;
+	console.log('scrolling from ' + window.scrollY + ' to ' + yTarget);
+	var scrollTickCoords = {x: window.scrollX, y: window.scrollY};
+	var offset;
+	var lead = 1;
+	if (interval) return false;
 
-// var animateScrollTo = function(x,y) {
-// 	var scrollTickCoords = {x: window.scrollX, y: window.scrollY};
-// 	var offset;
-// 	var lead = 1;
-// 	var interval = setInterval(function(){
-// 		if (y !== scrollTickCoords.y) {
-// 			offset = y - scrollTickCoords.y;
-// 			scrollTickCoords.y = Math.floor(scrollTickCoords.y + offset/20 + lead*Math.pow(offset,0));
-// 			console.log(scrollTickCoords.y);
-// 		} else {
-// 			clearInterval(interval);
-// 		}
-// 	},100);
-// };
+	interval = setInterval(function(){
+		offset = yTarget - scrollTickCoords.y;
+		var offsetSign = Math.pow(offset,0);
+
+		if (Math.abs(offset) >= 1) {
+			console.log(offset + '  ' + scrollTickCoords.y);
+			scrollTickCoords.y = scrollTickCoords.y + offset/10;
+			window.scrollTo(window.scrollX, scrollTickCoords.y);
+			// console.log(scrollTickCoords.y);
+		} else {
+			clearInterval(interval);
+			interval = null;
+			scrollDisabled = false;
+		}
+	},16);
+};
