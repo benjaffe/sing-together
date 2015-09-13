28ac5e2d53f1371ac59541eb6215b459dc0adfaf
Ben Jaffe <jaffe.ben@gmail.com>
2015-09-12 22:47:45 -0700

    big bunch of stuff for the client!

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,39 @@
+var Song = React.createClass({
+  render: function() {
+    var currentSong = this.props.currentSong;
+    if (!currentSong) {
+      return <div className="song-details">No Song Selected</div>;
+    }
+
+    return (
+      <div className="song-details">
+        <h2 className="song-title">Title: {currentSong.title}</h2>
+        <h3 className="song-artist">Artist: {currentSong.artist}</h3>
+        <div className="row">
+          <div className="col-sm-3 song-key">Key: {currentSong.key}</div>
+          <div className="col-sm-3 song-capo">Capo: {currentSong.capo}</div>
+          <div className="col-sm-3 song-readiness">Readiness: {currentSong.readiness}</div>
+          <div className="col-sm-3 song-whose">Whose: {currentSong.whose}</div>
+          <div className="col-sm-3 song-instruments">Instruments: {currentSong.instruments}</div>
+        </div>
+        <pre className="song-lyrics">Lyrics: {currentSong.lyrics}</pre>
+      </div>
+    );
+  }
+});
+
+var SongListItem = React.createClass({
+  render: function() {
+    return <li><a href={'#'+this.props.song.id}>{this.props.song.title}</a></li>;
+  }
+});
+
 var SongList = React.createClass({
   render: function() {
-    var createItem = function(itemText, index) {
-      return <li key={index + itemText}>{itemText}</li>;
+    var createItem = function(song, index) {
+      return <SongListItem song={song} />;
     };
-    return <ul>{this.props.songs.map(createItem)}</ul>;
+    return <ul className="song-list">{this.props.songs.map(createItem)}</ul>;
   }
 });
 
@@ -20,27 +50,27 @@ var SingSingApp = React.createClass({
       });
     }.bind(this));
   },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.songs.concat([this.state.text]);
-    var nextText = '';
-    this.setState({songs: nextItems, text: nextText});
-  },
   render: function() {
+    var currentSongID = this.props.songID;
+    var currentSong = _.find(this.state.songs, function(song){
+      return (song.id+'') === currentSongID;
+    }.bind(this));
+    console.log(currentSongID, currentSong);
     return (
       <div>
-        <h3>Sing Sing!</h3>
         <SongList songs={this.state.songs} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.songs.length + 1)}</button>
-        </form>
+        <Song currentSong={currentSong} />
       </div>
     );
   }
 });
 
-React.render(<SingSingApp />, document.getElementById('container'));
+function render() {
+  var route = window.location.hash.substr(1);
+  React.render(<SingSingApp songID={route} />, document.getElementById('container'));
+}
+
+window.addEventListener('hashchange', render);
+render();
+
+
